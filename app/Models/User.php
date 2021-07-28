82c86aee49da6feebb691f62f9eeb6e674837b6e
Muhammad Lutfi Ramadhan <muhammadlr17>
2021-07-28 11:20:44 +0700

    Create Models

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,9 +17,15 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name',
+        'first_name',
+        'last_name',
+        'username',
         'email',
+        'phone',
+        'company_id',
+        'departement_id',
         'password',
+        'role',
     ];
 
     /**
