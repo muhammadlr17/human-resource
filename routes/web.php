58568d5ae450da8e611f047386ebb0b135b3e565
Muhammad Lutfi Ramadhan <muhammadlr17>
2021-07-28 11:44:31 +0700

    Finishing Authentication Multi User

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +17,25 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Auth::routes();
+
+Route::middleware(['auth'])->group(function () {
+ 
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+ 
+    Route::middleware(['admin'])->group(function () {
+        Route::get('admin', [AdminController::class, 'index']);
+    });
+ 
+    Route::middleware(['user'])->group(function () {
+        Route::get('user', [UserController::class, 'index']);
+    });
+ 
+    Route::get('/logout', function() {
+        Auth::logout();
+        redirect('/');
+    });
+ 
 });
