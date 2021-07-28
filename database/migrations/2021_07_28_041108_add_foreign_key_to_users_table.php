aee31293e569eb8f1201db893dadde574670e45e
Muhammad Lutfi Ramadhan <muhammadlr17>
2021-07-28 11:15:27 +0700

    Finishing Migrations

---
+++ database/migrations/2021_07_28_041108_add_foreign_key_to_users_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddForeignKeyToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->unsignedBigInteger('departement_id')->after('phone');
+            $table->unsignedBigInteger('company_id')->after('phone');
+            $table->foreign('company_id')
+                ->on('companies')
+                ->references('id');
+            $table->foreign('departement_id')
+                ->on('departements')
+                ->references('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+}
